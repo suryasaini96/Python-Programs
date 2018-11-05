f9294a3e920fe1a669c1714577d60205c4ec9cd4
Surya Saini <34786382+suryasaini96@users.noreply.github.com>
2018-11-06 01:42:39 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+list = dict()
+numString=input("Enter the number")
+num=int(numString)
+
+for i in range(1,num+1):
+    list[i]=i*i
+
+print(list)
---
+++ listAndTuple.py
@@ -0,0 +1,10 @@
+
+
+values = input()
+eachValue=values.split(',')
+list=list(eachValue)
+tup=tuple(list)
+
+print(list)
+print("\n")
+print(tup)
